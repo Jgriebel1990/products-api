a8799d2d86428d63926877fb019e9d3484a8acee
jefferson Griebel <jefferson.griebel@gmail.com>
2018-03-14 19:30:08 -0400

    created mock product data and servered that as a GET request response

---
+++ mocks/products.js
@@ -0,0 +1,32 @@
+const products = [
+    {
+        _id: 'udhuiahrguhwgiufawiugfhq34hg98yt',
+        name: 'Javelin (caution)',
+        price: 500,
+        imgSrc: 'https://via.placeholder.com/100x100',
+        created: new Date()
+    },
+    {
+        _id: 'yfguiugyhbjntyfgvbhugyfvg',
+        name: 'Snail',
+        price: 3.99,
+        imgSrc: 'https://via.placeholder.com/100x100',
+        created: new Date()
+    },
+    {
+        _id: 'ur9guhiuhfuhiruhwiuehwiuehf',
+        name: 'Rug',
+        price: 99.99,
+        imgSrc: 'https://via.placeholder.com/100x100',
+        created: new Date()
+    }
+];
+
+
+
+
+
+
+
+
+module.exports = products;
