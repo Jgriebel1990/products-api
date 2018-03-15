a50894c7df9a11e1e0ba217ca019d6753af42223
jefferson Griebel <jefferson.griebel@gmail.com>
2018-03-15 19:36:19 -0400

    configured the use of environmental veriables and provided a template for the required variables

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const express = require('express');
+const express = require("express");
 const serverApp = express();
+
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5000; //necessary for Heroku deployment
 
 //routers
-const productRouter = require('./routers/products');
+const productRouter = require("./routers/products");
 
 serverApp.use(productRouter); //register the router with the application
 
-serverApp.get('/', (req, res) => {
-    res.send('i want some food maybe a fish taco or sandwich i also really want to eat this poptart');
+serverApp.get("/", (req, res) => {
+  res.send(
+    "i want some food maybe a fish taco or sandwich i also really want to eat this poptart"
+  );
 });
 
-
 serverApp.listen(PORT, () => {
-    console.log(`Now listening on port ${PORT}`);
+  console.log(`Now listening on port ${PORT}`);
 });
