3d9bd1e45590245c685fcb92efdf10d5f7824f15
jefferson Griebel <jefferson.griebel@gmail.com>
2018-03-14 20:41:56 -0400

    implemented POST route for products

--- routers/products.js
+++ routers/products.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mockProducts = require('../mocks/products');
+
 const productArrToObj = (arrayOfProducts) => {
     //create an accumulator object
     const accumulator = {};
@@ -31,6 +32,22 @@ router.get('/products/:id', (req, res) => {
         products: {
             [id]: selectedProducts
         }
+    })
+});
+//post means create
+router.post('/products', (req, res) => {
+    const productsObject = productArrToObj(mockProducts);
+    const id = 10000000 * Math.random();
+    const newProduct = {
+        _id: id,
+        name: 'something new',
+        price: '1000',
+        created: new Date(),
+        imgSrc: 'https://via.placeholder.com/100x100'
+    };
+    mockProducts.push(newProduct);
+    res.status(201).json({
+        msg: 'successfully created product'
     })
 });
 
