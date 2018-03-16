91cb64cad01ef79174597654f0c2fa6a79b12f71
jefferson Griebel <jefferson.griebel@gmail.com>
2018-03-15 20:06:33 -0400

    created the Product model and imported it into the router

---
+++ models/product.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    //name: String, //i would add more info this property
+    name: {
+        required: true,
+        type: String,
+    },
+    imgSrc: String,
+    price: Number,
+    created: {
+        required: true,
+        type: Date,
+        default: Date.now
+    },
+    description: String
+
+});
+
+const Product = mongoose.model('Product', productSchema);
+console.log(Product);
+module.exports = Product;
